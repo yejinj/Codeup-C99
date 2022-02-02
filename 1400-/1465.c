5be8ea39e23282902e9f2b3116311ad4655d03bd
[yejinj] <80540445+yejinj@users.noreply.github.com>
2022-02-02 18:28:12 +0900

    0202

---
+++ 1400-/1465.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+const int SIZE = 101;
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    int i, j;
+    for (i = a - 1; i >= 0; i--) {
+    	for (j = 1; j <= b; j++) {
+    		printf("%d ", b * i + j);
+		}
+		
+		printf("\n");
+	}
+    
+    return 0;
+}
---
+++ 1400-/1466.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	
+	int i, j, s = a * b;
+	for (i = 0; i < a; i++) {
+		for (j = 0; j < b; j++) {
+			printf("%d ", s - i - a * j);
+		}
+		
+		printf("\n");
+	}
+	
+	return 0;
+}
