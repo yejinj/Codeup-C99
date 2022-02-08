e9f26d3c5563d39dce95ed8ddd94775586ea4486
[yejinj] <80540445+yejinj@users.noreply.github.com>
2022-02-08 19:05:45 +0900

    0208

---
+++ 1400-/1474.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int arr[101][101] = {};   
+    
+    int a, b, i, j, x = 1, tmp = 1;
+    scanf("%d %d", &a, &b);
+    
+    for(i = b; i >= 1; i--) {
+        if(tmp % 2 == 0) {
+            for(j = 1; j <= a; j++) {
+                arr[j][i] = x;
+                x++;
+            }
+        }
+        else {
+            for(j = a; j>=1; j--) {
+                arr[j][i] = x;
+                x++;
+            }
+        }
+        
+        tmp++; 
+    }
+ 
+    for(i = 1; i <= a; i++) {
+        for(j = 1; j <= b; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
---
+++ 1500-/1503.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int arr[101][101] = {};   
+    
+	int a;
+	scanf("%d", &a);
+	
+	int i, j;
+	for (i = 1; i <= a; i++) {
+		for (j = 1; j <= a; j++) {
+			if (i % 2 == 1) {
+				arr[i][j] = a * (i - 1) + j;
+			}
+			
+			else {
+				arr[i][j] = a * i - j + 1;
+			}
+		}
+	}
+	
+	for (i = 1; i <= a; i++) {
+		for (j = 1; j <= a; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+    
+    return 0;
+}
