fa95316b8d2c129cfbab801af2b8028b34de4c3b
[yejinj] <80540445+yejinj@users.noreply.github.com>
2022-02-09 12:26:18 +0900

    0209

---
+++ 1000-/1098.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+ 
+int main()
+{
+    int arr[101][101] = {};
+    
+    int h, w, cnt;    
+    scanf("%d %d", &h, &w);
+    scanf("%d", &cnt);
+    
+    int i, j, l, d, x, y;
+    for(i = 1; i <= cnt; i++) {
+        scanf("%d %d %d %d", &l, &d, &x, &y);
+        
+        if(d == 0)
+        	for(j = 0; j < l; j++)
+            	arr[x][y + j] = 1;
+        else
+        	for(j = 0; j < l; j++)
+            	arr[x + j][y] = 1;
+    }
+    
+    for (i = 1; i <= h; i++) {
+        for (j = 1; j <= w; j++)
+        	printf("%d ", arr[i][j]);
+        printf("\n");
+    }
+    
+    return 0;
+}
---
+++ 1600-/1602.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+double absolute_v(double n) {
+	if (n >= 0)
+		return n;
+	else
+		return (-n);
+}
+ 
+int main()
+{
+    double n;
+    scanf("%lf", &n);
+    
+    printf("%.10g", absolute_v(n));
+    
+    return 0;
+}
