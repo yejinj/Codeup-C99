4d8528e36dc01171abb2a7a7c48eeb290e5c3a6d
[yejinj] <80540445+yejinj@users.noreply.github.com>
2022-02-13 21:34:25 +0900

    0213

---
+++ 1600-/1617.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+int f(int n)
+{
+	int num = 0;
+	while (n > 0) {
+		num *= 10;
+		num += n % 10;
+		n /= 10;
+	}
+	
+	return num;
+}
+
+int main( void)
+{
+
+	int inpt;
+	scanf("%d", &inpt);
+	
+	int ans = inpt + f(inpt);
+	if (f(ans) == ans)
+		printf("YES");	
+	else
+		printf("NO");
+	
+	return 0;
+
+}
