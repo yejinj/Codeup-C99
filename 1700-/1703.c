c37c4340f4906983bbaba5b5dca769875bf5c85f
[yejinj] <80540445+yejinj@users.noreply.github.com>
2022-02-14 17:27:18 +0900

    0214

---
+++ 1700-/1703.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main() {
+	int h = 0, m = 0, s = 0;
+	scanf("%d", &s);
+	
+	while (s >= 60) {
+		s -= 60;
+		m += 1;
+	}
+	
+	while (m >= 60) {
+		m -= 60;
+		h += 1;
+	}
+	
+	printf("%d %d %d", h, m, s);
+	
+	return 0;
+}
---
+++ 1700-/1707.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main() {
+	int arr[10];
+	
+	int i;
+	float mean = 0;
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &arr[i]);
+		mean += (arr[i] / 10.0);
+	}
+	
+	int c = 0;
+	for (i = 0; i < 10; i++)
+		if (arr[i] >= mean)
+			c++;
+			
+	printf("%.1f\n%d %d", mean, c, 10 - c);
+	
+	return 0;	
+}
